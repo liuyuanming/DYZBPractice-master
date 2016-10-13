02cbf38b448e12f230dbe912ba0d23d1f4deff6b
peter <1176175468@qq.com>
2016-10-13 15:11:19 +0800

    项目结构搭建

---
+++ DYZBPractice/DYZBPractice/Classes/Live/Controller/LiveViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LiveViewController.swift
+//  DYZBPractice
+//
+//  Created by peter on 16/10/13.
+//  Copyright © 2016年 peter. All rights reserved.
+//
+
+import UIKit
+
+class LiveViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
